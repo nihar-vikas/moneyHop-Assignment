e4a10a815038f44fe95a1169720b7a7072f53add
vikasap408 <vikas@appinessworld.com>
2022-06-22 13:14:01 +0530

    third commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import styles from './styles.module.css';
 import Logo from '../../assets/logo-nav.png';
 import { GetGlobalContext } from "../globalContext";
@@ -13,8 +13,11 @@ const Header = () => {
     const searchParm = new URLSearchParams(search).get('parms') || '';
     const { state, dispatch, fetchGif } = GetGlobalContext();
     const [suggestionList, setSuggestionList] = useState([]);
+    const [openList, setOpenList] = useState(false);
+    const dropdown = useRef(null);
 
     const fetchSearchSuggestions = async (searchVal) => {
+        if (!searchVal) return;
         try {
             const response = await fetch(`${state?.searchSuggestionURL}${searchVal}?api_key=${process.env.REACT_APP_ENV}`, {
                 method: 'GET',
@@ -23,7 +26,7 @@ const Header = () => {
                 }
             });
             const data = await response.json();
-            setSuggestionList(data?.data);
+            setSuggestionList(data?.data || []);
         } catch (error) {
             console.log(error?.response?.data?.message);
         }
@@ -34,19 +37,38 @@ const Header = () => {
         dispatch({ type: 'SET_SEARCH_VALUE', payload: searchParm });
     }, []);
 
-    const handleSearch = useCallback(debounce((e) => {
-        fetchGif(true, e.target.value);
-        if (!e.target.value) {
+    const handleSearch = (searchVal) => {
+        fetchGif(true, searchVal);
+        if (!searchVal) {
             setSuggestionList([]);
             navigation('/');
             return;
         }
-        navigation(`?parms=${e.target.value}`);
-    }, 500), []);
+        navigation(`?parms=${searchVal}`);
+    }
 
     const handleSuggestions = useCallback(debounce((e) => {
         fetchSearchSuggestions(e.target.value);
     }, 100), []);
+
+    useEffect(() => {
+        if (!openList) return;
+        function handleOutSideClick(event) {
+            if (dropdown.current && !dropdown.current.contains(event.target)) {
+                setOpenList(false);
+            }
+        }
+        window.addEventListener("click", handleOutSideClick);
+        return () => window.removeEventListener("click", handleOutSideClick);
+    }, [openList]);
+
+    const handleClear = (val) => {
+        if (!val && searchParm) {
+            fetchGif(true);
+            setSuggestionList([]);
+            navigation('/');
+        }
+    }
 
     return (
         <div className={styles.headerMain}>
@@ -59,23 +81,32 @@ const Header = () => {
                         autoCapitalize="off"
                         autoCorrect="off"
                         autoComplete="off"
+                        onKeyPress={(e) => e.key === 'Enter' ? handleSearch(state?.searchValue) : null}
                         className={styles.headerInputBox}
                         value={state?.searchValue}
                         placeholder="Search All GIFs"
+                        onFocus={() => setOpenList(true)}
+                        onBlur={(e) => handleClear(e.target.value)}
+                        onClick={() => setOpenList(true)}
                         onChange={(e) => {
+                            setOpenList(true);
                             dispatch({ type: 'SET_SEARCH_VALUE', payload: e.target.value.trimLeft() });
-                            handleSearch(e);
                             handleSuggestions(e);
                         }}
                     />
-                    <datalist autoComplete="off" id="searchBoxId" className={styles.dataList}>
-                        <select className={styles.dataList}>
-                            {suggestionList?.map((item, index) => (
-                                <option className={styles.dataListOption} value={item?.name} data-valueObj={item?.name} key={index}>{item?.name}</option>
+                    {suggestionList?.length > 0 && openList && state?.searchValue ? (
+                        <div ref={dropdown} className={styles.suggestList}>
+                            <h6 className={styles.suggestLabel}><img src={searchIcon} alt="searchIcon" height="15px" />&nbsp;&nbsp;&nbsp;Search suggestions</h6>
+                            {suggestionList.map((item, index) => (
+                                <div key={index} onClick={() => { dispatch({ type: 'SET_SEARCH_VALUE', payload: item?.name }); handleSearch(item?.name); setOpenList(false); }} className={styles.suggestListOption}>
+                                    <div className={styles.suggestListOptionContent}>
+                                        <h6 className={styles.suggestListName}>{item?.name}</h6>
+                                    </div>
+                                </div>
                             ))}
-                        </select>
-                    </datalist>
-                    <button className={styles.searchButton} disabled={!state?.searchValue} onClick={() => fetchGif(true, state?.searchValue)}>
+                        </div>
+                    ) : null}
+                    <button className={styles.searchButton} disabled={!state?.searchValue} onClick={() => handleSearch(state?.searchValue)}>
                         <img src={searchIcon} alt="searchIcon" />
                     </button>
                 </div>
